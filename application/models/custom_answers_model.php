c5051fe090a763abef23a716982ae89bdd2b926d
Hugo Carrasco <ranca.hugo@gmail.com>
2013-08-29 17:44:19 -0400

    fix

--- application/models/custom_answers_model.php
+++ application/models/custom_answers_model.php
@@ -12,13 +12,13 @@ class Custom_answers_model extends CI_Model
         $this->db->insert('custom_answers', array('answer' => $data['answer'], 'custom_questions_id' => $data['custom_questions_id'], 'apply_id' => $apply_id));
     }
 
-    function retrieve($custom_questions_id, $sex = "",$from_age = "",$to_age = "")
+    function retrieve($custom_questions_id, $sex = "",$from_age = 0,$to_age = 0)
     {
     	$this->db->select('answer,sex,YEAR(CURDATE()) - YEAR(birth_date) as age');
     	$this->db->where('custom_questions_id', $custom_questions_id);
         if($sex != "")
             $this->db->where('sex', $sex);
-        if(($from_age != "" && $to_age != "") && ($from_age <= $to_age))
+        if(($from_age != 0 && $to_age != 0) && ($from_age <= $to_age))
         {
             $this->db->having('age >=', $from_age);
             $this->db->having('age <=', $to_age);
